3971ddcb70605a4720c434c37c1c0cfc2272750d
Yure Rafael <yurerafael01@gmail.com>
2024-09-30 15:48:08 -0300

    feat: create projects section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { About } from "./components/About";
 import { Header } from "./components/Header";
 import { Home } from "./components/Home";
+import { Projects } from "./components/Projects";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
      <Header/>
      <Home/>
      <About/>
+     <Projects/>
     </>
   )
 }
