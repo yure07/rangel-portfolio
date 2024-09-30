ab1e600245b2f60bfd781fddafa3e97b4e685534
Yure Rafael <yurerafael01@gmail.com>
2024-09-30 17:22:56 -0300

    feat: create contact section

---
+++ src/components/Contact/index.tsx
@@ -0,0 +1,24 @@
+import { LinkedinIcon, MailIcon, GlobeIcon } from 'lucide-react'
+
+export const Contact = () => {
+  return(
+    <section className="flex flex-col items-center w-80 lg:w-96 h-96 bg-[#262626] shadow-portfolio mt-24 mx-auto text-white py-12 px-12 text-center">
+      <div>
+        <span className="text-xl">Fale comigo</span>
+        <h1 className="font-black italic uppercase text-4xl">contato</h1>
+      </div>
+      <div className="flex flex-row items-center self-start italic text-base mt-12 gap-4">
+        <LinkedinIcon size={30}/>
+        <span>linkedin/user-name</span>
+      </div>
+      <div className="flex flex-row items-center self-start italic text-base my-8 gap-4">
+        <MailIcon size={30}/>
+        <span>email@email.com</span>
+      </div>
+      <div className="flex flex-row items-center self-start italic text-base gap-4">
+        <GlobeIcon size={30}/>
+        <span>gbrangel.com</span>
+      </div>
+    </section>
+  )
+}
