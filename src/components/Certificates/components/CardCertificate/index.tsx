a5d3a4b3df7a62323a98114fb2d20ffc9d427d1e
Yure Rafael <yurerafael01@gmail.com>
2024-10-15 15:18:43 -0300

    style: turn card certificate responsive

--- src/components/Certificates/components/CardCertificate/index.tsx
+++ src/components/Certificates/components/CardCertificate/index.tsx
@@ -7,10 +7,10 @@ interface CardCertificateProps{
 export const CardCertificate = (certificateProp: CardCertificateProps) => {
   return(
     <div className="flex flex-col lg:flex-row gap-6 my-8 mx-auto items-center text-center">
-      <div className="w-52 h-52 lg:w-44 lg:h-44 bg-[#1E1E1E] shadow-portfolio"/>
+      <div className="w-48 h-48 lg:w-44 lg:h-44 bg-[#1E1E1E] shadow-portfolio"/>
       <div className="flex flex-col gap-2 lg:text-start">
-        <h3 className="uppercase text-xl font-black">{certificateProp.title}</h3>
-        <p className="text-justify text-xs w-52 lg:w-64 px-2 lg:p-0">{certificateProp.description}</p>
+        <h3 className="uppercase text-base lg:text-xl font-black">{certificateProp.title}</h3>
+        <p className="text-justify text-xs w-48 lg:w-64 px-2 lg:p-0">{certificateProp.description}</p>
       </div>
     </div>
   )
